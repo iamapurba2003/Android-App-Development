fdf28fe6b8f7f9f91789a7e0fc126716eb969ba8
Dust Of Snow <new.to.world2003@gmail.com>
2022-04-16 16:12:16 +0530

    Added a Card View and Elevation of that with Proper Alignment of a avater into that only ... More to Come

--- BuisnessCard/app/src/main/java/tech/fersatech/buisnessCard/MainActivity.kt
+++ BuisnessCard/app/src/main/java/tech/fersatech/buisnessCard/MainActivity.kt
@@ -3,13 +3,24 @@ package tech.fersatech.buisnessCard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tech.fersatech.buisnessCard.ui.theme.BuisnessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +29,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             BuisnessCardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateBizCard()
                 }
             }
         }
@@ -27,14 +41,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 5.dp
+        ) {
+
+            Column(modifier = Modifier.height(300.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+                Surface(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(5.dp),
+                    shape = CircleShape,
+                    border = BorderStroke(0.5.dp, Color.LightGray),
+                    elevation = 4.dp,
+                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+                )
+                {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile_image),
+                        contentDescription = "Profile Image",
+                        modifier = Modifier.size(135.dp),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+            }
+
+
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BuisnessCardTheme {
-        Greeting("Android")
+        CreateBizCard()
     }
 }
